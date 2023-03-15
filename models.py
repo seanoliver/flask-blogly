67df63e703f64c415b8c12e13c1b8ef3ebfbab7c
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-15 12:29:41 -0700

    wrote user model

--- models.py
+++ models.py
@@ -1,1 +1,44 @@
-"""Models for Blogly."""
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from models import db, connect_db, Pet
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///flask_blogly')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User:
+    """User model."""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    first_name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    image_url = db.Column(
+        db.Text,
+        nullable=True)
